a679dac35769465134f861060ed95fbb6e5f3bbb
Hafez <a.hafez852@gmail.com>
2022-04-17 18:32:06 +0200

    perf: improve solution by generating only half of both arrays re 4.median of sorted arrays

--- src/4.median-of-two-sorted-arrays.ts
+++ src/4.median-of-two-sorted-arrays.ts
@@ -6,14 +6,34 @@
 
 // @lc code=start
 function findMedianSortedArrays(nums1: number[], nums2: number[]) {
-  const sortedArray = nums1.concat(nums2).sort((a, b) => a - b);
-  const middleIndex = Math.floor(sortedArray.length / 2);
-
-  if (sortedArray.length % 2 === 0) {
-    return (sortedArray[middleIndex] + sortedArray[middleIndex - 1]) / 2;
+  const { halfMergedArray, arraysTotalLength } = mergeSortedArrays(nums1, nums2);
+  if (arraysTotalLength % 2 === 0) {
+    return (halfMergedArray[halfMergedArray.length - 2] + halfMergedArray[halfMergedArray.length - 1]) / 2;
   }
 
-  return sortedArray[middleIndex];
+  return halfMergedArray[halfMergedArray.length - 1];
 }
 // @lc code=end
 export { findMedianSortedArrays };
+
+function mergeSortedArrays(nums1: number[], nums2: number[]) {
+  const arraysTotalLength = nums1.length + nums2.length;
+
+  let index1 = 0;
+  let index2 = 0;
+  const halfMergedArray = [];
+
+  while (halfMergedArray.length <= arraysTotalLength / 2) {
+    if (
+      (nums1[index1] !== undefined && nums1[index1] < nums2[index2]) ||
+       nums2[index2] === undefined
+    ) {
+      halfMergedArray.push(nums1[index1]);
+      index1++;
+    } else {
+      halfMergedArray.push(nums2[index2]);
+      index2++;
+    }
+  }
+  return { halfMergedArray, arraysTotalLength };
+}
