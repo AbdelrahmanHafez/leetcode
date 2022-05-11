844011a762055b751947c59758a5810063f12cf3
Hafez <a.hafez852@gmail.com>
2022-05-11 17:32:42 +0200

    add solution for zigzag conversion

--- src/6.zigzag-conversion.ts
+++ src/6.zigzag-conversion.ts
@@ -5,39 +5,51 @@
  */
 
 // @lc code=start
-function convert(s: string, numRows: number) {
-  const matrix = createMatrixFromString(s, numRows);
-
-  let zizaggedString = '';
-  for (const row of matrix) {
-    for (const character of row) {
-      if (character !== undefined) {
-        zizaggedString += character;
-      }
-    }
+function convert(string: string, numRows: number) {
+  if (numRows === 1) {
+    return string;
   }
-  return zizaggedString;
-}
+  const matrix = createMatrix(numRows);
+  fillMatrix(string, numRows, matrix);
 
-
-function createMatrixFromString(string: string, numRows: number) {
-  const matrix = new Array(numRows).map(() => []);
-
-  const currentColumnIndex = 0;
-  const currentRowIndex = 0;
-
-  for (let i = 0; i < string.length; i++) {
-    const character = string[i];
-    matrix[currentRowIndex][currentColumnIndex] = character;
-
-    if (currentRowIndex === 0) {
-
-    }
-  }
-
-  return matrix;
+  const zigzaggedString = convertMatrixToString(matrix);
+  return zigzaggedString;
 }
 
 // @lc code=end
 
 export { convert as convertToZigzag };
+
+function fillMatrix(string: string, numRows: number, matrix: any[]) {
+  let currentRowIndex = 0;
+  let directionIndex = -1;
+
+  for (const character of string) {
+    const isFirstOrLastRow = currentRowIndex === 0 || currentRowIndex === (numRows - 1);
+    if (isFirstOrLastRow) {
+      directionIndex *= -1;
+    }
+
+    matrix[currentRowIndex].push(character);
+
+    currentRowIndex += directionIndex;
+  }
+}
+
+function createMatrix(numRows: number) {
+  const matrix = [];
+
+  for (let i = 0; i < numRows; i++) {
+    matrix[i] = [];
+  }
+  return matrix;
+}
+
+function convertMatrixToString(matrix: string[][]) {
+  let result = '';
+
+  for (let i = 0; i < matrix.length; i++) {
+    result += matrix[i].join('');
+  }
+  return result;
+}
