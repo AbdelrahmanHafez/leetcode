8632814f1a20bb0475a8312a0d6443207f745967
Hafez <a.hafez852@gmail.com>
2025-02-19 00:04:00 +0100

    fix: utilize linked list for adding two numbers

--- src/2.add-two-numbers.ts
+++ src/2.add-two-numbers.ts
@@ -8,44 +8,46 @@ class ListNode {
   val: number;
   next: ListNode | null;
   constructor(val?: number, next?: ListNode | null) {
-    this.val = (val === undefined ? 0 : val);
-    this.next = (next === undefined ? null : next);
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
   }
 }
 
 // @lc code=start
-function addTwoNumbers(l1: ListNode | null, l2: ListNode | null) {
-  const n1Stringified = convertListNodeToReversedStringifiedNumber(l1);
-  const n2Stringified = convertListNodeToReversedStringifiedNumber(l2);
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
 
-  const reversedSum = BigInt(n1Stringified) + BigInt(n2Stringified);
-  return convertReversedNumberToList(reversedSum);
-}
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  const root = new ListNode(0);
+  let currentNode = root;
+  let carry = 0;
 
+  while (l1 != null || l2 != null || carry !== 0) {
+    const sum = (l1?.val || 0) + (l2?.val || 0) + carry;
+    carry = Math.floor(sum / 10);
 
-function convertListNodeToReversedStringifiedNumber(listNode: ListNode) {
-  let stringifiedNumber = '';
-  let currentNode = listNode;
-
-  while (currentNode) {
-    stringifiedNumber = `${currentNode.val}${stringifiedNumber}`;
+    currentNode.next = new ListNode(sum % 10);
     currentNode = currentNode.next;
+    if (l1) {
+      l1 = l1.next;
+    }
+    if (l2) {
+      l2 = l2.next;
+    }
   }
-
-  return stringifiedNumber;
-}
-
-function convertReversedNumberToList(number: BigInt) {
-  const digits = String(number).split('').reverse().join('');
-  const rootNode = new ListNode(Number(digits[0]));
-  let currentNode = rootNode;
-
-  for (const digit of digits.slice(1)) {
-    currentNode.next = new ListNode(Number(digit));
-    currentNode = currentNode.next;
-  }
-
-  return rootNode;
+  return root.next;
 }
 
 // @lc code=end
